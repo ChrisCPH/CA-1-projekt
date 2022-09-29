52d4531cfabfde14ae9a569a36fb9d3dc17d2fee
jonathanbraad <jonathanbraad123@gmail.com>
2022-09-29 14:16:13 +0200

    personfacade

--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -1,2 +1,18 @@
-public class MainTest {
+import facades.PersonFacade;
+
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+public class MainTest
+{
+    public static void main(String[] args)
+    {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("pu");
+        PersonFacade personFacade = PersonFacade.getPersonFacade(emf);
+
+
+        System.out.println(personFacade.getPersonByEmail("asd"));
+
+
+    }
 }
